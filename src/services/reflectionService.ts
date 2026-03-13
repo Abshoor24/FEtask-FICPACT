0a6af61bb1a1d8f0fabcbe378bb9d2d574014c74
SiamAlSobari <siamqueen52@gmail.com>
2026-03-13 16:04:17 +0700

    fix: memperbaiki err build type

--- src/services/reflectionService.ts
+++ src/services/reflectionService.ts
@@ -2,16 +2,16 @@ import { apiClient } from "@/common/libs/api-client";
 import { CreateUserReflection, UserFailedReflection } from "@/data/models/reflectionModel";
 
 class RefelectionService {
-    public async createUserReflection(data: CreateUserReflection): Promise<{data: CreateUserReflection}> {
-        return await apiClient<{data: CreateUserReflection}>({
+    public async createUserReflection(data: CreateUserReflection): Promise<{ data: CreateUserReflection } | null> {
+        return await apiClient<{ data: CreateUserReflection }>({
             url: "/reflection/quest",
             method: "POST",
             data
         });
     }
 
-    public async createUserFailed(data: UserFailedReflection): Promise<{data: UserFailedReflection}> {
-        return await apiClient<{data: UserFailedReflection}>({
+    public async createUserFailed(data: UserFailedReflection): Promise<{ data: UserFailedReflection } | null> {
+        return await apiClient<{ data: UserFailedReflection }>({
             url: "/reflection/create-failed",
             method: "POST",
             data
