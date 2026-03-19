685e57ed5230592b25281f60685bc617b2a7e4fa
SiamAlSobari <siamqueen52@gmail.com>
2026-03-19 16:22:52 +0700

    feat: Create initial Next.js root layout with Geist fonts, QueryProvider, and Toaster setup.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import QueryProvider from "@/common/providers/QueryProvider";
-
+import toast, { Toaster } from 'react-hot-toast';
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -32,6 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <QueryProvider>{children}</QueryProvider>
+        <Toaster />
       </body>
     </html>
   );
